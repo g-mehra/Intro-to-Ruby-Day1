046637ce41cbeb105ec5f5fbafd37bd1431443cc
g-mehra <gmehra@alumni.uwo.ca>
2017-01-16 13:15:19 -0500

    Exercise 2 complete

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+=begin
+#Solution1
+puts "How much was your meal?"
+meal_cost = gets.chomp
+tip = meal_cost.to_f * 1.13 * 0.13 #Assumes tip should be 13% of after tax cost of meal
+puts "$#{tip.round(2)} is a decent tip."
+
+
+#Solution2
+puts "Hi " + 4.to_s
+
+#Solution3
+puts "45628 multiplied by 7839 is #{45628*7839}"
+
+#Solution 4
+puts "Expression is true!"
+
+=end
