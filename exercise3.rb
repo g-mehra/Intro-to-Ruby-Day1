e481405fccbdb6bd4633e415b85bb319afc8d1b1
g-mehra <gmehra@alumni.uwo.ca>
2017-01-16 13:32:58 -0500

    Add code to determine year born

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,8 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+
+puts "How old are you?"
+age = gets.chomp
+year_born = Time.now.year - age.to_i
+puts "That means you were born in #{year_born} (+/- 1 year depending on the month in which you were born)!"
